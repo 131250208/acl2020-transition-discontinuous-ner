9990f916635232b235f122cee50be9ee8db833a0
MaskAdam <wychengpublic@163.com>
2021-01-23 08:01:52 +0800

    eval

---
+++ code/xdai/ner/transition_discontinuous/eval.sh
@@ -0,0 +1,2 @@
+python ../evaluate.py --gold_filepath ../../../../data/dai031/Experiments/CADEC/adr/split/test.txt \
+--pred_filepath ../../../../data/dai031/Experiments/TransitionDiscontinuous/cadec/52/test.pred
